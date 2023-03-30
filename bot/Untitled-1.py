695779c95b53230f531cadec44589321f00f043d
Arten Kress <artemkress1@gmail.com>
2023-03-30 23:12:32 +0300

    revier

--- bot/Untitled-1.py
+++
@@ -1,9 +0,0 @@
-try:
-    x=int(input("введіть число x>0"))
-except ValueError:
-    print("ви ввели число менше 0")
-    x=0
-else:
-    print("число більше 0")
-finally:
-    print("дякую за огляд")
